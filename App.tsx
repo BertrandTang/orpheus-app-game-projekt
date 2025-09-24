56cb57134d7b56573f8be4ee4dd15e442de459a4
Bertrand <bertrand.tang.dev@gmail.com>
2025-09-24 16:53:33 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HeaderBackButton } from "@react-navigation/elements";
+const Stack = createNativeStackNavigator();
+import Home from "./screens/Home";
+import Game from "./screens/Game";
+import Result from "./screens/Results";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Game" component={Game} />
+        <Stack.Screen
+          name="Result"
+          component={Result}
+          options={({ navigation }) => ({
+            headerLeft: (props) => (
+              <HeaderBackButton
+                {...props}
+                label="Home"
+                onPress={() => navigation.navigate("Home")}
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/Game.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, StatusBar, TouchableHighlight, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Game() {
+    const [choice, setChoice] = useState('');
+    const baseNumber = Math.floor(Math.random() * 100);
+    const score = Math.floor(Math.random() * 100);
+
+    console.log(`beginning base Number = ${baseNumber} et score = ${score}`);
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        if (choice) {
+            console.log(`log in useeffect base Number = ${baseNumber} et score = ${score}`);
+            const winner =
+                (choice === 'higher' && score > baseNumber) ||
+                (choice === 'lower' && baseNumber > score);
+
+            Alert.alert(
+                'You\'ve got the winner ?',
+                winner ? 'won' : 'lost',
+                [{
+                    text: `You scored: ${score}`,
+                    onPress: () => navigation.goBack(),
+                }]
+            );
+        }
+    }, [baseNumber, score, choice]);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+            <Text>Starting: {baseNumber}</Text>
+
+            <TouchableHighlight
+                onPress={() => setChoice('higher')}
+                style={[styles.button, styles.buttonGreen]}
+            >
+                <Text style={styles.buttonText}>Higher</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight
+                onPress={() => setChoice('lower')}
+                style={[styles.button, styles.buttonRed]}
+            >
+                <Text style={styles.buttonText}>Lower</Text>
+            </TouchableHighlight>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    baseNumber: {
+        fontSize: 40,
+        marginBottom: 30,
+    },
+    button: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        borderRadius: 10,
+        padding: 30,
+        marginVertical: 15,
+    },
+    buttonRed: {
+        backgroundColor: 'red',
+    },
+    buttonGreen: {
+        backgroundColor: 'green',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 24,
+    },
+});
